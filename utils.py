051e832571e177be50dae6d4eca9d8a84b5225bd
MAKARnikanov <maka.nik.00@mail.ru>
2023-02-14 10:25:04 +0300

    Created a code so that the program does not crush

--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
 import requests
 from pprint import pprint
 def get_data(url):
-    response = requests.get(url).json()
-    pprint(response)
+    response = requests.get(url)
+    try:
+        if response.status_code == 200:
+            return response.json(), "INFO: Данные получены успешно"
+    except requests.exceptions.ConnectionError():
+        return None, "ERROR: requests.exceptions.ConnectionError"
+    except requests.exceptions.JSONDecodeError():
+        return None, "ERROR: requests.exceptions.JSONDecodeError"
+
